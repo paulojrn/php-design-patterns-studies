8f3ce1b921a1307d0cf81dd0974d3176ebc3f292
Paulo JRN <paulo.xcco@gmail.com>
2022-03-21 20:07:51 -0300

    Behavioral/State: code to refactor

--- src/Budget.php
+++ src/Budget.php
@@ -2,8 +2,29 @@
 
 namespace Study\DesignPattern;
 
+use DomainException;
+
 class Budget
 {
     public int $itemAmount;
     public float $value;
+    public string $status;
+
+    public function applyExtraDiscount()
+    {
+        $this->value = $this->calculateExtraDiscount();
+    }
+
+    public function calculateExtraDiscount()
+    {
+        if ($this->status === "EM_APROVACAO") {
+            return $this->value * 0.05;
+        }
+
+        if ($this->state === "APROVADO") {
+            return $this->value * 0.02;
+        }
+
+        throw new DomainException("Orçamentos aprovados e finalizados não podem recerber descontos");
+    }
 }
